d054544182ec31132aa8870f24150b99454313f8
Seif-01 <sifreyassin@gmail.com>
2023-12-31 17:20:07 +0100

    variables 0 error

---
+++ C/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+int x; //declaration
+x = 101; // intialization
+int y = 111; // declaration + intialization
+
+int age = 23; //integer
+float height = 174.5; //float
+char bloodtype = 'B';  //single character
+char country[] = "canada"; //array of character
+    return 0;
+}
