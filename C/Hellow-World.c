f9a3646963a65d4cc79005d0a20f24e8cd77a611
Seif-01 <sifreyassin@gmail.com>
2023-12-31 16:56:12 +0100

    multi comments

--- C/Hellow-World.c
+++ C/Hellow-World.c
@@ -2,6 +2,10 @@
 
 int main(){
 // this gonna print what its typed between the double quotes
+/*comment
+another comment
+so fun
+yeah*/
 printf("i live in canada\n");
 printf("its very cold!\n");
 return 0;
