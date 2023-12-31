3d7fdd3d6e8975bb2e41a369647d0287f90f60bf
Seif-01 <sifreyassin@gmail.com>
2023-12-31 17:28:23 +0100

    fixed

--- C/variables.c
+++ C/variables.c
@@ -13,7 +13,7 @@ char country[] = "canada"; //array of character
    
    printf("you living in %s\n",country);
    printf("you are %.1f cm\n" ,height);
-   prinf("your blood type is %c" ,bloodtype);
+   printf("your blood type is %C" ,bloodtype);
 
 return 0;
 }
