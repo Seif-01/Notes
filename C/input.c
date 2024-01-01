c5a9ed33d8aed33d6ce330850c80a79fbe92b311
Seif-01 <sifreyassin@gmail.com>
2024-01-01 16:47:33 +0100

    input error

--- C/input.c
+++ C/input.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main (){
- int name;
+ int name[10];
  printf("Enter Your Name :");
- scanf("%s", &"name");
- printf("Your name is %s","name");
+ scanf("%s", &name);
+ printf("Your name is %s",name);
 
     return 0;
 }
