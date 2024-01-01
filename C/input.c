aa674d6c652a3db82cc38bd32223c276d018c1f4
Seif-01 <sifreyassin@gmail.com>
2024-01-01 16:53:50 +0100

    Segmentation fault (core dumped)

--- C/input.c
+++ C/input.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main (){
- int name[10];
+ char name[100];
  printf("Enter Your Name :");
- scanf("%s", &name);
+ scanf("%s",name);
  printf("Your name is %s",name);
 
     return 0;
