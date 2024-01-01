1173e5c89098c50637089e38ee23fab666372f55
Seif-01 <sifreyassin@gmail.com>
2024-01-01 16:13:16 +0100

    fixed

--- C/printf.c
+++ C/printf.c
@@ -4,6 +4,6 @@ int main (){
 
 printf("%d\t", 900); //print number
 printf("%s\n","seconds"); //print text
-printf("printing a this number %.1f and this character %c in same lane %.1f %c","g",50.5 );
+printf("printing a this number %.1f and this character \"%c\" in same lane ",50.5,'C' );
     return 0;
 }
