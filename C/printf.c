267749c304d94844123bf98db001dabc30f5147e
Seif-01 <sifreyassin@gmail.com>
2024-01-01 16:07:48 +0100

    error 1

--- C/printf.c
+++ C/printf.c
@@ -4,5 +4,6 @@ int main (){
 
 printf("%d\t", 900); //print number
 printf("%s","seconds"); //print text
+printf("printing a this number (float) %f and this character %c in same lane %f %c",'g',50.5 );
     return 0;
 }
