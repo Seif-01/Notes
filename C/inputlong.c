f0c5bdbfb1baec940d14c15f531d0ce6ae11549e
Seif-01 <sifreyassin@gmail.com>
2024-01-01 17:31:44 +0100

    worked

---
+++ C/inputlong.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+ char fullname[20];l;
+ printf("Enter Your Full Name :");
+ fgets(fullname,20,stdin);
+ printf("Your Full Name is %s\n",fullname);
+    return 0;
+}
