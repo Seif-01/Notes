31940e0b7cc7007a7d625d20b4e4a913ff24712a
Seif-01 <sifreyassin@gmail.com>
2024-01-01 16:19:20 +0100

    easy

--- C/printf.c
+++ C/printf.c
@@ -2,7 +2,7 @@
 
 int main (){
 
-printf("%d\t", 900); //print number
+printf("%d\t", -900); //print number
 printf("%s\n","seconds"); //print text
 printf("printing a this number %.1f and this character \"%c\" in same lane ",50.5,'C' );
     return 0;
