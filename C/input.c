3f094c73c5d9b70f53138f18b258ba185855e053
Seif-01 <sifreyassin@gmail.com>
2024-01-01 17:19:08 +0100

    stupid code it worked

--- C/input.c
+++ C/input.c
@@ -3,9 +3,9 @@
 int main ()
 {
  char name[20];
- printf("Enter Your Name :");
- scanf("%s", name);
- printf("Your name is %s",name);
+ printf("Enter Your Name :\n");
+ scanf("%s\n", name);
+ printf("Your name is %s \n",name);
 
     return 0;
 }
