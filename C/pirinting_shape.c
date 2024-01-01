3283049dd78a385eebe3ba594e01e36f38f1f085
Seif-01 <sifreyassin@gmail.com>
2024-01-01 15:34:50 +0100

    bingo

---
+++ C/pirinting_shape.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+printf (" ______    ______   _____    _____\n");
+printf ("|      |  |           |     |     \n");
+printf ("|______   |______     |     |____ \n");
+printf ("       |  |           |     |     \n");
+printf ("|______|  |______   __|__   |     \n");
+    return 0; 
+}
